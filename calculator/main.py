1025214e128148a8991da97440237e3d27cdd582
nmdeni <mekadeni@mail.ru>
2024-04-08 12:45:57 +0300

    transfer project

--- calculator/main.py
+++ calculator/main.py
@@ -1,18 +1,28 @@
 a = float(input("first num > "))
 b = float(input("second num > "))
+command_list = ['+','-','/','*','**']
+
+print('Commands' + str(command_list))
+
 c = input("what will we do > ")
 res = None
 
-if c == '+':
-    res = a + b
-elif c == '-':
-    res = a - b
-elif c == '*':
-    res = a * b
-elif c == ':' or c == '/':
-    res = a / b
-else:
-    print("Not found command")
+for i in command_list:
+    if c == i:
+        if c == '+':
+            res = a + b
+        elif c == '-':
+            res = a - b
+        elif c == '*':
+            res = a * b
+        elif c == '/':
+            res = a / b
+        elif c == '**':
+            res = a ** b
 
-print("Task > " + str(int(a)) + " " + str(c) + " " + str(int(b)))
-print("This is result = " + str(int(res)))
+        print("Task > " + str(int(a)) + " " + str(c) + " " + str(int(b)))
+        print("This is result = " + str(int(res)))
+        break
+    else:
+        continue
+
