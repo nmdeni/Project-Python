92fe2928bff4532d740e0f00ab7434d20792218a
nmdeni <mekadeni@mail.ru>
2024-04-09 15:31:29 +0300

    wrote the first instructions

---
+++ input_survey/main.py
@@ -0,0 +1,11 @@
+user_date = []
+user_name = input('insert your name -> ')
+user_lastname = input('insert your lastname ->')
+user_age = input('insert your age ->')
+
+user_date.append(user_name.capitalize())
+user_date.append(user_lastname.capitalize())
+user_date.append(user_age)
+
+
+print("\n".join(user_date))
