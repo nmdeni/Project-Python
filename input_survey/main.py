3dc4aba5cfc2433ab070089dd0549ecc2d6a3303
nmdeni <mekadeni@mail.ru>
2024-04-10 12:20:33 +0300

    edited the outpud info for user

--- input_survey/main.py
+++ input_survey/main.py
@@ -1,10 +1,10 @@
 user_date = []
 user_name = input('insert your name -> ')
-user_lastname = input('insert your lastname ->')
-user_age = input('insert your age ->')
+user_lastname = input('insert your lastname -> ')
+user_age =input('insert your age -> ')
 
-user_date.append(user_name.capitalize())
-user_date.append(user_lastname.capitalize())
-user_date.append(user_age)
+user_date.append('Name - ' + user_name.capitalize())
+user_date.append('Lastname - ' + user_lastname.capitalize())
+user_date.append( 'Age - ' + user_age)
 
 print("\n".join(user_date))
