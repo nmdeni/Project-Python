bc9fa0e56a9875df48f03010d6625f983fb1a012
nmdeni <mekadeni@mail.ru>
2024-04-11 14:00:32 +0300

    i created list with necessary data

--- input_survey/main.py
+++ input_survey/main.py
@@ -1,1 +1,12 @@
-user_date = {}
+user_date = {
+    'user_id':'',
+    'user_name':'',
+    'user_lastname':'',
+    'user_age':0,
+    'user_work':'',
+    'user_hobby':[]
+}
+
+for key,val in user_date.items():
+    print(key + ' -> ' + str(val))
+    print(type(val))
