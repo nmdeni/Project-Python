87248639b449b8c4bf7c7afac9f9ded6b019c6b2
nmdeni <mekadeni@mail.ru>
2024-04-11 14:20:56 +0300

    i created main cycle for write info out of user

--- input_survey/main.py
+++ input_survey/main.py
@@ -2,11 +2,15 @@ user_date = {
     'user_id':'',
     'user_name':'',
     'user_lastname':'',
-    'user_age':0,
+    'user_age':'',
     'user_work':'',
     'user_hobby':[]
 }
-
+#
 for key,val in user_date.items():
-    print(key + ' -> ' + str(val))
-    print(type(val))
+    if key == 'user_id':
+        user_date[key] = (1)
+    else:
+        user_date[key] = input('insert ' + str(key) + ' -> ')
+
+print(user_date)
