da8e5ab82f2b260633a9f0f363bae7090d308aba
nmdeni <mekadeni@mail.ru>
2024-04-26 19:46:21 +0300

    начал реализовывать работы программы на стадии меню

--- input_survey/main.py
+++ input_survey/main.py
@@ -6,7 +6,6 @@ def add_user():
     pass
 def rem_user():
     pass
-
 def edit_user():
     pass
 
@@ -15,21 +14,36 @@ user_date = {
     'user_name':'',
     'user_lastname':'',
     'user_age':'',
-    'user_status':'',
+    'user_status':'', #переделать под зависимость (пример = мама => дочери)
     'user_work':'',
     'user_info':''
 }
 
-for key,val in user_date.items():
-    if key == 'user_id':
-        user_date[key] = (1)
-    elif key == 'user_name' or key == 'user_lastname':
-        user_date[key] = input('insert ' + str(key) + ' -> ').title()
-    else:
-        user_date[key] = input('insert ' + str(key) + ' -> ')
+start_prog = True
 
-print("*** USER INFORMATION ***")
-for key,val in user_date.items():
-    print(f"{key.replace('user_','').title()} => {val}")
+# for key,val in user_date.items():
+#     if key == 'user_id':
+#         user_date[key] = (1)
+#     elif key == 'user_name' or key == 'user_lastname':
+#         user_date[key] = input('Введите ' + str(key) + ' -> ').title()
+#     else:
+#         user_date[key] = input('Введите ' + str(key) + ' -> ')
+#
+# print(f"*** ИНФОРМАЦИЯ О {user_date['user_name']} ***")
+# for key,val in user_date.items():
+#     print(f"{key.replace('user_','').title()} => {val}")
 
-view_tree()
+while start_prog:
+    command = [
+        "1 - Вывести все древо",
+        "2 - Вывести инфо человека",
+        "3 - Добавить человека",
+        "4 - Удалить человека",
+        "5 - Редактировать информацию",
+        "q - Выход"
+    ]
+    print("\n".join(command) + "\n")
+
+    user_com = input('Введите команду => ')
+    if user_com.lower() == "q":
+        break
