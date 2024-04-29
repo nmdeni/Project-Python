09ec27c65d61669125ab8aff0f363fa422d748db
nmdeni <mekadeni@mail.ru>
2024-04-29 19:09:11 +0300

    организовал работу менюшки

--- learning_map/main.py
+++ learning_map/main.py
@@ -1,5 +1,18 @@
 def start():
-    print('Hello черт')
+    user_com = ''
+    menu = [
+        'q - Выход',
+        '1 - Поменять статус предмета/уровень',
+        '2 - Удалить предмет/уровень',
+        '3 - Редактировать предмет/уровень'
+    ]
+    for i in menu:
+        print(i)
+
+    while user_com.lower() != 'q':
+        user_com = input('Введите команду => ')
+        if user_com not in menu:
+            print('!!!Нет такой команды (команды = q,1,2,3)!!!')
 
 if __name__ == "__main__":
     start()
