70a6dd4a8ccb535aace78aabff56dff2fac68fa8
nmdeni <mekadeni@mail.ru>
2024-05-02 12:43:11 +0300

    добавил раскрас текста при выводе и протестировал все робит збс

--- learning_map/lmap.py
+++ learning_map/lmap.py
@@ -1,6 +1,9 @@
 import json
+import colorama
+from colorama import Fore,Back,Style
 
 def view_lmap(ldata):
+    colorama.init()
     with open(ldata,encoding='utf-8') as f:
         f_data = json.load(f)
         str_data = ""
@@ -10,6 +13,12 @@ def view_lmap(ldata):
 
             for level,programm in value.items():
                 str_data += f"Уровень {level.replace('level_','')}\n\t\t"
-                str_data += f"Предметы {" | ".join(programm)}\n\t"
+
+                str_data += "Предметы "
+                for i in programm:
+                    str_data += (f"{Fore.GREEN if "+" in i else ''}"
+                                 f"{Fore.YELLOW if "#" in i else ''}"
+                                 f"{i}{Style.RESET_ALL} | ")
+                str_data +="\n\t"
 
         print(str_data)
