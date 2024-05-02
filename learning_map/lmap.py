f6b9cba222411f47fbee91272329501966e30598
nmdeni <mekadeni@mail.ru>
2024-05-02 11:58:02 +0300

    подраправил вывод устранил ошибки кодировки при выводе

--- learning_map/lmap.py
+++ learning_map/lmap.py
@@ -1,13 +1,15 @@
 import json
 
 def view_lmap(ldata):
-    with open(ldata) as f:
+    with open(ldata,encoding='utf-8') as f:
         f_data = json.load(f)
         str_data = ""
         for discip,value in f_data.items():
-            str_data += f"Дисциплина {discip}\n\t"
+            str_data.strip()
+            str_data += f"\r*** Дисциплина {discip.strip()} ***\n\t"
 
             for level,programm in value.items():
                 str_data += f"Уровень {level.replace('level_','')}\n\t\t"
                 str_data += f"Предметы {" | ".join(programm)}\n\t"
+
         print(str_data)
