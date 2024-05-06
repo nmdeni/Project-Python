65c25871a74fb64f5fd133887170047a9e829391
nmdeni <mekadeni@mail.ru>
2024-05-06 13:06:06 +0500

    добавил инструкции для полного экрана

--- aliens_game/main.py
+++ aliens_game/main.py
@@ -10,6 +10,9 @@ class AliceInvasion():
         pygame.init()
         self.settings = Settings()
 
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_width()
+        # self.settings.screen_height = self.screen.get_height()
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption('Alice Invasion')
 
