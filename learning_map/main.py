c55c235753770df711743257a845109f1943e825
nmdeni <mekadeni@mail.ru>
2024-05-06 21:35:56 +0500

    начала проведения рефакторинга с изменения модуля lmap и переобивки структура программы на ООП

--- learning_map/main.py
+++ learning_map/main.py
@@ -1,36 +1,41 @@
-import lmap
+from view_lmap import view_lmap
 
-def start():
-    user_com = ''
-    learn_data = 'learning_data.json'
-    menu = [
-        'q - Выход',
-        '1 - Поменять статус предмета',
-        '2 - Удалить предмет',
-        '3 - Редактировать предмет',
-        '4 - Добавить предмет'
-    ]
+class LearningMap():
+    def __init__(self, file_name):
+        self.file_data = file_name
+        self.user_com = ''
+        self.menu = [
+            'q - Выход',
+            '1 - Поменять статус предмета',
+            '2 - Удалить предмет',
+            '3 - Редактировать предмет',
+            '4 - Добавить предмет'
+        ]
 
-    #ВЫВОД КАРТА
-    lmap.view_lmap(learn_data)
-    for i in menu:
-        print(i)
+    def run(self):
+        # ВЫВОД КАРТА
+        view_lmap(self.file_data)
+        for i in self.menu:
+            print(i)
 
-    while user_com.lower() != 'q':
-        user_com = input('Введите команду => ')
-        if user_com == "q":
-            print("Завершение программы...")
-        elif user_com == '1':
-            pass
-        elif user_com == '2':
-            del_elem = lmap.del_elem_lmap(learn_data)
-            print(f"{del_elem} был удален!!!")
-        elif user_com == '3':
-            pass
-        elif user_com == '4':
-            print(lmap.add_elem_lmap(learn_data))
-        else:
-            print('!!!Нет такой команды (команды = q,1,2,3)!!!')
+        while self.user_com.lower() != 'q':
+            self.user_com = input('Введите команду => ')
+            if self.user_com == "q":
+                print("Завершение программы...")
+            elif self.user_com == '1':
+                pass
+            elif self.user_com == '2':
+                # del_elem = lmap.del_elem_lmap(learn_data)
+                # print(f"{del_elem} был удален!!!")
+                pass
+            elif self.user_com == '3':
+                pass
+            elif self.user_com == '4':
+                pass
+                # print(lmap.add_elem_lmap(learn_data))
+            else:
+                print('!!!Нет такой команды (команды = q,1,2,3)!!!')
 
 if __name__ == "__main__":
-    start()
+    lmap = LearningMap('learning_data.json')
+    lmap.run()
